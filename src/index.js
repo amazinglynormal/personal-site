5cefb9f4a5c8b76d0f0a4a38345ba373f306ff73
Stephen <stephenbrady365@gmail.com>
2025-11-20 08:12:22 +0000

    add mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("connected");
+const closeMobileMenu = document.querySelector(".mobile-menu__button-close");
+const openMobileMenu = document.querySelector(".mobile-menu__button-open");
+const mobileMenuOverlay = document.querySelector(".mobile-menu__overlay");
+const mobileMenu = document.querySelector(".mobile-menu__overlay");
+
+openMobileMenu.addEventListener("click", () => {
+    mobileMenu.classList.add("open");
+    mobileMenu.classList.remove("close");
+});
+
+closeMobileMenu.addEventListener("click", () => {
+    mobileMenu.classList.remove("open");
+    mobileMenu.classList.add("close");
+});
+
+mobileMenuOverlay.addEventListener("click", function (event) {
+    if (this === event.target) {
+        mobileMenu.classList.remove("open");
+        mobileMenu.classList.add("close");
+    }
+});
